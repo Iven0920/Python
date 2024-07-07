9b8d92b391208fb44ca0e6b88af08545f0874bd5
Iven <905284811@qq.com>
2024-07-07 17:12:33 +0800

    Iven

---
+++ 011_字符串格式化.py
@@ -0,0 +1,20 @@
+class_num = 60
+avg_salary = 1244
+message = "一个班一共%s人，一个年级有%s名同学" % (class_num, avg_salary)
+print(message)
+
+name = "Iven"
+message = "我是%s" % name
+print(message)
+
+"""
+占位符
+%s 将内容转换成字符串
+# %d 将内容转换成整数
+# %f 将内容转换成浮点型
+"""
+
+name = "Iven"
+age = 21
+gpa = 3.59
+print("我的名字是:%s ,我的年龄为：%d岁, 我的GPA为:%f" % (name, age, gpa))
